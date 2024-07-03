e1c73a279f788cae8a8c297ed2c60f11c31df16e
qazxsw1240 <qazxsw010528@gmail.com>
2024-07-03 17:20:38 +0900

    update NutritionFacts

--- src/main/java/org/hansung/oddeco/core/entity/nutrition/NutritionFacts.java
+++ src/main/java/org/hansung/oddeco/core/entity/nutrition/NutritionFacts.java
@@ -1,9 +1,25 @@
 package org.hansung.oddeco.core.entity.nutrition;
 
 public interface NutritionFacts extends Iterable<NutritionFacts.Info> {
+    public static NutritionFacts of() {
+        return new NutritionFactsImpl();
+    }
+
+    public static NutritionFacts of(NutritionFacts other) {
+        return new NutritionFactsImpl(other);
+    }
+
+    public static NutritionFacts of(int carbohydrate, int protein, int fat, int vitamin) {
+        return new NutritionFactsImpl(carbohydrate, protein, fat, vitamin);
+    }
+
     public abstract int getNutrition(Nutrition nutrition);
 
     public static interface Info {
+        public static Info of(Nutrition nutrition, int amount) {
+            return new NutritionFactsImpl.InfoImpl(nutrition, amount);
+        }
+
         public abstract Nutrition getNutrition();
 
         public abstract int getAmount();
