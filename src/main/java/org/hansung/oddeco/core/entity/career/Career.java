81f81b600e10b18ab687ebb9732dc7272d6955e3
qazxsw1240 <qazxsw010528@gmail.com>
2024-07-05 21:52:11 +0900

    update Career

--- src/main/java/org/hansung/oddeco/core/entity/career/Career.java
+++ src/main/java/org/hansung/oddeco/core/entity/career/Career.java
@@ -1,5 +1,9 @@
 package org.hansung.oddeco.core.entity.career;
 
+import org.bukkit.entity.Player;
+
 public interface Career {
+    public abstract Player getPlayer();
+
     public abstract CareerCoins getCoins( );
 }
