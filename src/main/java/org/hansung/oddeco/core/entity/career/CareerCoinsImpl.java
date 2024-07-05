bb19531345391b2a8e29b1725620fa1bd7336668
qazxsw1240 <qazxsw010528@gmail.com>
2024-07-05 22:09:17 +0900

    implement Career && CareerCoins

---
+++ src/main/java/org/hansung/oddeco/core/entity/career/CareerCoinsImpl.java
@@ -0,0 +1,23 @@
+package org.hansung.oddeco.core.entity.career;
+
+class CareerCoinsImpl implements CareerCoins {
+    private volatile int coins;
+
+    public CareerCoinsImpl() {
+        this(0);
+    }
+
+    public CareerCoinsImpl(int coins) {
+        this.coins = coins;
+    }
+
+    @Override
+    public int getCoins() {
+        return this.coins;
+    }
+
+    @Override
+    public void setCoins(int coins) {
+        this.coins = coins;
+    }
+}
