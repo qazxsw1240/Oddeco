71964c5e98c7641feb4e67d5e43321586aa77821
Artiren <sketchhydrogenmagnesium@gmail.com>
2024-07-06 18:37:15 +0900

    add register HunterListener to Oddeco

--- src/main/java/org/hansung/oddeco/core/hunter/HunterListener.java
+++ src/main/java/org/hansung/oddeco/core/hunter/HunterListener.java
@@ -4,7 +4,6 @@ import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerVelocityEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class HunterListener implements Listener {
@@ -12,16 +11,13 @@ public class HunterListener implements Listener {
 
     public HunterListener(JavaPlugin plugin) {
         this.plugin = plugin;
+        plugin.getLogger().info("HunterListener Registed.");
     }
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
+        plugin.getLogger().info("Player Joined.");
         LivingEntity player = event.getPlayer();
         player.knockback(0.000000000000001, -0.1, -0.1);
-    }
-
-    @EventHandler
-    public void onPlayerVelocity(PlayerVelocityEvent event) {
-        event.setCancelled(true);
     }
 }
