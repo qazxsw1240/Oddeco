58d48a5e9b9225f3b8c52b31b85e733132c5a9ee
qazxsw1240 <qazxsw010528@gmail.com>
2024-07-07 04:43:11 +0900

    add CareerPermissionBuilderImpl

---
+++ src/main/java/org/hansung/oddeco/core/entity/career/CareerPermissionBuilderImpl.java
@@ -0,0 +1,59 @@
+package org.hansung.oddeco.core.entity.career;
+
+class CareerPermissionBuilderImpl implements CareerPermissionBuilder {
+    private volatile Career career;
+    private volatile long id;
+    private volatile String name;
+    private volatile String description;
+
+    public CareerPermissionBuilderImpl() {
+        this.career = null;
+        this.id = 0L;
+        this.name = null;
+        this.description = null;
+    }
+
+    @Override
+    public CareerPermissionBuilder setCareer(Career career) {
+        this.career = career;
+        return this;
+    }
+
+    @Override
+    public CareerPermissionBuilder setId(long id) {
+        this.id = id;
+        return this;
+    }
+
+    @Override
+    public CareerPermissionBuilder setId(String id) {
+        this.id = Long.parseLong(id);
+        return this;
+    }
+
+    @Override
+    public CareerPermissionBuilder setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    @Override
+    public CareerPermissionBuilder setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    @Override
+    public CareerPermission build() {
+        if (this.career == null) {
+            throw new IllegalStateException("career not set");
+        }
+        if (this.name == null) {
+            throw new IllegalStateException("name not set");
+        }
+        if (this.description == null) {
+            throw new IllegalStateException("description not set");
+        }
+        return null;
+    }
+}
