dc9482a3a7535e22f1bead0847b533db912bfc21
qazxsw1240 <qazxsw010528@gmail.com>
2024-07-07 02:04:24 +0900

    update CareerCoins

--- src/main/java/org/hansung/oddeco/core/entity/career/CareerCoins.java
+++ src/main/java/org/hansung/oddeco/core/entity/career/CareerCoins.java
@@ -1,6 +1,14 @@
 package org.hansung.oddeco.core.entity.career;
 
 public interface CareerCoins {
+    public static CareerCoins of() {
+        return new CareerCoinsImpl();
+    }
+
+    public static CareerCoins of(int coins) {
+        return new CareerCoinsImpl(coins);
+    }
+
     public abstract int getCoins();
 
     public abstract void setCoins(int coins);
