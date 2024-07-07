535b3e84b1c91c37b9f729a2afb6226ee8556c93
Artiren <sketchhydrogenmagnesium@gmail.com>
2024-07-07 12:25:01 +0900

    add Hunter

---
+++ src/main/java/org/hansung/oddeco/core/hunter/Hunter.java
@@ -0,0 +1,11 @@
+package org.hansung.oddeco.core.hunter;
+
+public class Hunter {
+    private final int level;
+
+    public Hunter(int level) {
+        this.level = level;
+    }
+
+    public int getLevel() { return level; }
+}
--- src/main/java/org/hansung/oddeco/core/hunter/HunterListener.java
+++ src/main/java/org/hansung/oddeco/core/hunter/HunterListener.java
@@ -1,23 +1,49 @@
 package org.hansung.oddeco.core.hunter;
 
+import com.destroystokyo.paper.event.entity.EntityKnockbackByEntityEvent;
+import org.bukkit.Material;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
+import org.hansung.oddeco.core.util.logging.FormattedLogger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 public class HunterListener implements Listener {
-    JavaPlugin plugin;
+    private static final Logger log = LoggerFactory.getLogger(HunterListener.class);
+    private final JavaPlugin plugin;
+    private final FormattedLogger logger;
+    private final ConcurrentMap<LivingEntity, Hunter> hunters;
 
-    public HunterListener(JavaPlugin plugin) {
+
+    public HunterListener(JavaPlugin plugin, FormattedLogger logger) {
         this.plugin = plugin;
-        plugin.getLogger().info("HunterListener Registed.");
+        this.logger = logger;
+        this.hunters = new ConcurrentHashMap<>();
+        logger.info("HunterListener Registed.");
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        hunters.put(event.getPlayer(), new Hunter(1));
     }
 
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        plugin.getLogger().info("Player Joined.");
-        LivingEntity player = event.getPlayer();
-        player.knockback(0.000000000000001, -0.1, -0.1);
+    public void onEntityDeath(EntityDeathEvent event) {
+        Player player = event.getEntity().getKiller();
+        if (hunters.containsKey(player)) {
+            player.heal(1);
+        }
     }
 }
