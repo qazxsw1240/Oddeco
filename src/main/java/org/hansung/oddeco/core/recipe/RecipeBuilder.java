ea55d018d12b7c3f8daad96ebe6ec96b006a6391
Artiren <sketchhydrogenmagnesium@gmail.com>
2024-07-16 18:22:53 +0900

    add RecipeBuilder

---
+++ src/main/java/org/hansung/oddeco/core/recipe/RecipeBuilder.java
@@ -0,0 +1,67 @@
+package org.hansung.oddeco.core.recipe;
+
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+public class RecipeBuilder {
+    private NamespacedKey key;
+    private ItemStack item;
+    private String[] shape;
+    private final ConcurrentMap<Character, Material> ingredients;
+
+    public RecipeBuilder() {
+        this.shape = new String[3];
+        this.ingredients = new ConcurrentHashMap<>();
+    }
+
+    public RecipeBuilder(NamespacedKey key) {
+        this();
+        this.key = key;
+    }
+
+    public RecipeBuilder(ItemStack item, NamespacedKey key) {
+        this(key);
+        this.item = item;
+    }
+
+    // add all line's shape to recipe
+    public void setShapes(String[] shape) {
+        this.shape = shape;
+    }
+
+    // add one line's shape to recipe
+    public void addShape(int line, String shape) {
+        if (line >= this.shape.length)
+            throw new RuntimeException(String.format("line 값은 0부터 2 사이의 값을 가지지만, %d로 지정되었습니다.", line));
+        this.shape[line] = shape;
+    }
+
+    // set all ingredients to recipe
+    public void setIngredients(Map<Character, Material> ingredients) {
+        this.ingredients.putAll(ingredients);
+    }
+
+    // add one ingredient to recipe
+    public void addIngredient(Character key, Material value) {
+        this.ingredients.put(key, value);
+    }
+
+    // set item of recipe result
+    public void setItem(ItemStack item) {
+        this.item = item;
+    }
+
+    public ShapedRecipe build() {
+        if (item == null) throw new NullPointerException("레시피로 제작할 아이템이 지정되지 않았습니다.");
+        if (key == null) key = item.getType().getKey();
+        ShapedRecipe recipe = new ShapedRecipe(key, item).shape(shape); // set recipe's shape
+        ingredients.forEach((key, value) -> recipe.setIngredient(key, value)); // set ingredients
+        return recipe;
+    }
+}
