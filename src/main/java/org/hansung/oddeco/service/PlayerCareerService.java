d0abc3ca2d904000a4f8dce5a948246b06b1ada5
qazxsw1240 <qazxsw010528@gmail.com>
2024-07-19 21:03:29 +0900

    update PlayerCareerService

--- src/main/java/org/hansung/oddeco/service/PlayerCareerService.java
+++ src/main/java/org/hansung/oddeco/service/PlayerCareerService.java
@@ -1,34 +1,34 @@
 package org.hansung.oddeco.service;
 
+import org.bukkit.advancement.Advancement;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerAdvancementDoneEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.Plugin;
-import org.hansung.oddeco.core.sql.AbstractStatementExecutor;
+import org.hansung.oddeco.core.entity.career.CareerCoins;
 import org.hansung.oddeco.core.util.logging.FormattedLogger;
+import org.hansung.oddeco.repository.AdvancementRewardRepository;
 import org.hansung.oddeco.repository.CareerCoinsRepository;
 
-import java.sql.*;
-
-public class PlayerCareerService extends AbstractStatementExecutor implements Listener {
+public class PlayerCareerService implements Listener {
     private final Plugin plugin;
     private final CareerCoinsRepository careerCoinsRepository;
+    private final AdvancementRewardRepository advancementRewardRepository;
     private final FormattedLogger logger;
 
     public PlayerCareerService(
             Plugin plugin,
             CareerCoinsRepository careerCoinsRepository,
-            Connection connection,
+            AdvancementRewardRepository advancementRewardRepository,
             FormattedLogger logger) {
-        super(connection);
         this.plugin = plugin;
         this.careerCoinsRepository = careerCoinsRepository;
+        this.advancementRewardRepository = advancementRewardRepository;
         this.logger = logger;
-
-        initializeSqlTable();
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
@@ -49,32 +49,19 @@ public class PlayerCareerService extends AbstractStatementExecutor implements Li
     public void onPlayerQuit(PlayerQuitEvent event) {
     }
 
-    private void initializeSqlTable() {
-        try {
-            DatabaseMetaData databaseMetaData = this.connection.getMetaData();
-            ResultSet resultSet = databaseMetaData.getTables(null, null, "player_career_coins", new String[]{"TABLE"});
-            try (resultSet) {
-                if (!resultSet.next()) {
-                    try (Statement statement = this.connection.createStatement()) {
-                        statement.execute("""
-                                           create table player_career_coins
-                                           (
-                                               uuid   varchar not null constraint player_nutrition_pk primary key,
-                                               amount integer not null
-                                           )
-                                          """);
-                        statement.execute("""
-                                          create unique index player_career_coins_uuid_uindex
-                                              on player_career_coins (uuid)
-                                          """);
-                        this.logger.info("Successfully create table 'player_career_coins'");
-                    }
-                } else {
-                    this.logger.info("found existing table 'player_career_coins'");
-                }
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onPlayerMadeAdvancement(PlayerAdvancementDoneEvent event) {
+        Player player = event.getPlayer();
+        Advancement advancement = event.getAdvancement();
+        CareerCoins careerCoins = this.careerCoinsRepository
+                .get(player)
+                .orElseThrow();
+        this.advancementRewardRepository.get(advancement)
+                .ifPresent(reward -> {
+                    int coins = reward.getReward() + careerCoins.getCoins();
+                    careerCoins.setCoins(coins);
+                    this.logger.info("Successfully player gained %d of career coins", reward.getReward());
+                    this.logger.info("Now player has %d of career coins", careerCoins.getCoins());
+                });
     }
 }
