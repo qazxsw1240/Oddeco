8dd6c300e1a618d2f669e2a1b79f80efe268f3bf
qazxsw1240 <qazxsw010528@gmail.com>
2024-07-19 17:28:49 +0900

    update Oddeco

--- src/main/java/org/hansung/oddeco/Oddeco.java
+++ src/main/java/org/hansung/oddeco/Oddeco.java
@@ -2,6 +2,9 @@ package org.hansung.oddeco;
 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.FlushModeType;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.Style;
 import net.kyori.adventure.text.format.TextDecoration;
@@ -14,11 +17,13 @@ import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.hansung.oddeco.butcher.ButcherListener;
 import org.hansung.oddeco.core.Plugin;
-import org.hansung.oddeco.hunter.HunterListener;
+import org.hansung.oddeco.core.entity.nutrition.PlayerNutritionFactRewardData;
+import org.hansung.oddeco.core.entity.player.PlayerNutritionStateData;
 import org.hansung.oddeco.core.json.JsonUtil;
 import org.hansung.oddeco.core.util.entity.ItemStackBuilder;
 import org.hansung.oddeco.core.util.logging.FormattedLogger;
 import org.hansung.oddeco.core.util.logging.PluginLoggerFactory;
+import org.hansung.oddeco.hunter.HunterListener;
 import org.hansung.oddeco.repository.AdvancementRewardRepository;
 import org.hansung.oddeco.repository.NutritionFactRepository;
 import org.hansung.oddeco.repository.PlayerNutritionFactRewardDataRepository;
@@ -26,26 +31,28 @@ import org.hansung.oddeco.repository.PlayerNutritionRepository;
 import org.hansung.oddeco.service.PlayerAdvancementRewardService;
 import org.hansung.oddeco.service.PlayerNutritionService;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.time.Duration;
-import java.util.concurrent.atomic.AtomicReference;
 
 @Plugin
 public final class Oddeco extends JavaPlugin {
     private static final String DB_URL = "jdbc:sqlite:plugins/Oddeco/database.db";
 
+    private static final Class<?>[] ENTITIES = new Class[]{
+            PlayerNutritionStateData.class,
+            PlayerNutritionFactRewardData.class
+    };
+
     private final FormattedLogger logger;
-    private final AtomicReference<Connection> connection;
 
     private final NutritionFactRepository nutritionFactRepository;
     private final AdvancementRewardRepository advancementRewardRepository;
 
+    private final EntityManagerFactory entityManagerFactory;
+    private final EntityManager entityManager;
+
     public Oddeco() throws ClassNotFoundException {
         Class.forName("org.sqlite.JDBC");
         this.logger = PluginLoggerFactory.getLogger(this);
-        this.connection = new AtomicReference<>();
 
         JsonObject object = JsonUtil
                 .of("/nutrition_fact_data.json")
@@ -57,40 +64,27 @@ public final class Oddeco extends JavaPlugin {
 
         this.nutritionFactRepository = new NutritionFactRepository(object);
         this.advancementRewardRepository = new AdvancementRewardRepository(array);
+        this.entityManagerFactory = new OddecoEntityManagerBuilder(DB_URL, ENTITIES);
+        this.entityManager = this.entityManagerFactory.createEntityManager();
+        this.entityManager.setFlushMode(FlushModeType.AUTO);
     }
 
     @Override
     public void onDisable() {
-        try {
-            Connection connection = this.connection.get();
-            if (connection != null) {
-                connection.close();
-            }
-            this.logger.info("DB Connection successfully closed");
-            this.logger.info("Disabled Plugin [Oddeco]");
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        this.entityManager.close();
+        this.entityManagerFactory.close();
+        this.logger.info("Disabled Plugin [Oddeco]");
     }
 
     @Override
     public void onEnable() {
-        try {
-            Connection connection = DriverManager.getConnection(DB_URL);
-            this.connection.set(connection);
-            this.logger.info("DB Connection successfully established");
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
         this.logger.info("Enabled Plugin [%s]", getName());
-        Connection connection = this.connection.get();
 
         PlayerNutritionService playerNutritionService = new PlayerNutritionService(
                 this,
                 this.nutritionFactRepository,
-                new PlayerNutritionRepository(connection),
-                new PlayerNutritionFactRewardDataRepository(connection),
-                connection,
+                new PlayerNutritionRepository(this.entityManager),
+                new PlayerNutritionFactRewardDataRepository(this.entityManager),
                 this.logger);
 
         playerNutritionService.setExhaustionDecrement(3);
